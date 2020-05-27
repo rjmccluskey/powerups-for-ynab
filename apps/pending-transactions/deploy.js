0d6142f491f531a6c83d10708776761293cff8e4
rjmccluskey <rjmccluskey@gmail.com>
2020-05-26 23:21:50 -0700

    Configure initial deployment of pending-transactions

---
+++ apps/pending-transactions/deploy.js
@@ -0,0 +1,36 @@
+'use strict';
+const fs = require('fs');
+const { exec } = require("child_process");
+const packageJson = require('../../package.json');
+
+const simplePackageJson = {
+  name: 'pending-transactions',
+  version: '0.0.0',
+  // Just copy all dependencies for now. Eventually this should be smarter
+  // so it only uses dependencies needed for the app
+  dependencies: packageJson.dependencies
+};
+
+const jsonString = JSON.stringify(simplePackageJson);
+fs.writeFileSync(
+  'dist/apps/pending-transactions/package.json',
+  jsonString
+);
+
+const gcloudCommand = `
+gcloud functions deploy pending-transactions \\
+  --entry-point=main \\
+  --source=dist/apps/pending-transactions/ \\
+  > /dev/null
+`;
+
+console.log('Deploying pending-transactions...');
+exec(gcloudCommand, (error, stdout) => {
+  if (stdout) {
+    console.log(stdout);
+  }
+  if (error) {
+    throw error;
+  }
+  console.log('pending-transactions deployed!');
+});
