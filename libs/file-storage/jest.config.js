cb30b7682bd4ba8cf865e7cae772c5f0a7d0cf1e
Rich McCluskey <rich@dozuki.com>
2020-11-07 12:50:27 -0800

    Update Nx
    These were automated changes after running `nx migrate latest`

--- libs/file-storage/jest.config.js
+++ libs/file-storage/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: 'file-storage',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/file-storage'
+  coverageDirectory: '../../coverage/libs/file-storage',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'file-storage',
 };
