1532a5a65704e1daec2b4c0986f81afeedc9983b
Rich McCluskey <rich@dozuki.com>
2020-11-17 21:07:29 -0800

    Add beginning of track-by-balance main function with test for local debugging

--- apps/track-by-balance/src/main.ts
+++ apps/track-by-balance/src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import { mapEveryAccount, ynabApi } from '@pfy/ynab-utils';
+
+export const main = async () => {
+  const ynab = ynabApi();
+  await mapEveryAccount(ynab, async (account) => {
+    console.log(account);
+  });
+};
