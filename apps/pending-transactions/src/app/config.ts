6862e10025251f291d67b924ab934cc73e8a2155
Rich McCluskey <rich@dozuki.com>
2020-11-25 20:24:41 -0800

    Rename env var so Nx can find it

--- apps/pending-transactions/src/app/config.ts
+++ apps/pending-transactions/src/app/config.ts
@@ -11,5 +11,5 @@ export const config: Config = {
   nodeEnv: getEnvVar('NODE_ENV', NodeEnv.prod) as NodeEnv,
   useTracing: getEnvVar('USE_TRACING', 'false') === 'true',
   debugBucketName: getEnvVar('DEBUG_BUCKET_NAME', null),
-  plaidAccessToken: getEnvVar('PLAID_ACCESS_TOKEN'),
+  plaidAccessToken: getEnvVar('NX_PLAID_ACCESS_TOKEN'),
 };
