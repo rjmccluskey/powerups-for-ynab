fe6e76d7fd848f730dbee3c3b84739b3418c6034
Rich McCluskey <rich@dozuki.com>
2021-07-06 22:18:27 -0700

    Optimize track-by-balance function
    Now this gathers all plaid account a balances upfront so that it doesn't
    loop over all ynab accounts multiple times for each plaid account.

--- apps/track-by-balance/src/main.ts
+++ apps/track-by-balance/src/main.ts
@@ -2,63 +2,64 @@ import { mapEveryAccount, ynabApi, ynabErrorWrapper } from '@pfy/ynab-utils';
 import { plaidClient } from '@pfy/plaid-utils';
 import { floatToMilliunits, getEnvVar, now } from '@pfy/utils';
 import { SaveTransaction } from 'ynab';
-import { AccountsResponse, Client, PlaidError } from 'plaid';
+import { AccountsResponse, PlaidError } from 'plaid';
 
 export const main = async () => {
-  const plaid = plaidClient();
   const ynab = ynabApi();
-  const accessTokens = JSON.parse(getEnvVar('NX_BALANCE_ACCESS_TOKENS'));
-  for (const accessToken of accessTokens) {
-    const accountNameToBalance = await mapBalancesByAccountName(
-      plaid,
-      accessToken
-    );
-    await mapEveryAccount(ynab, async (account, budget) => {
-      const accountName = account.note;
-      const currentBalance = accountNameToBalance[accountName];
-      if (currentBalance !== undefined && account.balance !== currentBalance) {
-        const difference = currentBalance - account.balance;
-        console.log(
-          `Uploading adjustment transaction to ${accountName} for ${difference}`
-        );
-        await ynab.transactions
-          .createTransaction(budget.id, {
-            transaction: {
-              account_id: account.id,
-              date: now().toISODate(),
-              amount: difference,
-              payee_name: 'Automatic balance adjustment',
-              memo: 'Sent from Powerups for YNAB',
-              cleared: SaveTransaction.ClearedEnum.Cleared,
-              approved: true,
-            },
-          })
-          .catch(ynabErrorWrapper);
-      }
-    });
-  }
+  const accountNameToBalance = await mapBalancesByAccountName();
+  await mapEveryAccount(ynab, async (account, budget) => {
+    const accountName = account.note;
+    const currentBalance = accountNameToBalance[accountName];
+    if (currentBalance !== undefined && account.balance !== currentBalance) {
+      const difference = currentBalance - account.balance;
+      console.log(
+        `Uploading adjustment transaction to ${accountName} for ${difference}`
+      );
+      await ynab.transactions
+        .createTransaction(budget.id, {
+          transaction: {
+            account_id: account.id,
+            date: now().toISODate(),
+            amount: difference,
+            payee_name: 'Automatic balance adjustment',
+            memo: 'Sent from Powerups for YNAB',
+            cleared: SaveTransaction.ClearedEnum.Cleared,
+            approved: true,
+          },
+        })
+        .catch(ynabErrorWrapper);
+    }
+  });
 };
 
-async function mapBalancesByAccountName(
-  plaid: Client,
-  accessToken: string
-): Promise<{ [name: string]: number }> {
-  let accountsResponse: AccountsResponse;
-  try {
-    accountsResponse = await plaid.getAccounts(accessToken);
-  } catch (e) {
-    if (e instanceof PlaidError && e.error_code === 'ITEM_LOGIN_REQUIRED') {
-      const lastSix = accessToken.substr(accessToken.length - 6);
-      console.error(`Login required on access token ending in ...${lastSix}`);
-      return {};
-    }
-    throw e;
-  }
+async function mapBalancesByAccountName(): Promise<{ [name: string]: number }> {
+  const plaid = plaidClient();
+  const accessTokens = JSON.parse(getEnvVar('NX_BALANCE_ACCESS_TOKENS'));
 
-  return accountsResponse.accounts.reduce((all, account) => {
-    all[account.name] = floatToMilliunits(account.balances.current);
-    return all;
-  }, {});
+  const mappings = await Promise.all(
+    accessTokens.map(async (accessToken) => {
+      let accountsResponse: AccountsResponse;
+      try {
+        accountsResponse = await plaid.getAccounts(accessToken);
+      } catch (e) {
+        if (e instanceof PlaidError && e.error_code === 'ITEM_LOGIN_REQUIRED') {
+          const lastSix = accessToken.substr(accessToken.length - 6);
+          console.error(
+            `Login required on access token ending in ...${lastSix}`
+          );
+          return {};
+        }
+        throw e;
+      }
+
+      return accountsResponse.accounts.reduce((all, account) => {
+        all[account.name] = floatToMilliunits(account.balances.current);
+        return all;
+      }, {});
+    })
+  );
+
+  return Object.assign({}, ...mappings);
 }
 
 // Uncomment when testing locally
